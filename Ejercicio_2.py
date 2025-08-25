f4facc4cbf8988d11e6d14461a476437880c9b79
david pedraza <davlpedraza@soy.sena.edu.co>
2025-08-25 09:38:38 -0500

    hacer una simulacion de guardar,cargar y salir de manera de selecion

---
+++ Ejercicio_2.py
@@ -0,0 +1,29 @@
+def juego():
+    """
+    en la clase de juego user es donde se almacenan el menu para la selecion del usuario
+    con match seleciona opcion
+    mostrandola como mensaje
+    :return:
+    case 1
+    case 2
+    case 3
+    """
+    while True:
+            user_input = int(input("Ingresar 1 para guardar\n"
+                                   "Ingresar 2 para cargar\n"
+                                   "Ingresar 3 para ssalir\n"
+                                   "Ingresar opcion: "
+
+            ))
+            match user_input:
+                case 1:
+                    print("Guardado correctamente")
+                case 2:
+                    print("Cargado correctamente")
+                case 3:
+                    print("saliendo correctamente")
+                    break
+                case _:
+                    print("opcion invalida")
+if __name__ == "__main__":
+    juego()
